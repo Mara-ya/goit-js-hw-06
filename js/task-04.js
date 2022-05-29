8e60a06b919b5aa3ec330060b1bcade2dcfd4968
Mara-ya <97354812+Mara-ya@users.noreply.github.com>
2022-05-29 19:31:39 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,14 @@
 const subtractionBtn = document.querySelector('[data-action="decrement"]');
 const additionBtn = document.querySelector('[data-action="increment"]');
-let counterValue = 0;
-
 const value = document.querySelector('#value');
-
+let counterValue = 0;
 
 function onSubtractionBtnClick(event) {
     value.textContent = --counterValue;
-    console.log(value);
 };
 subtractionBtn.addEventListener('click', onSubtractionBtnClick);
 
 function onAdditionBtnClick(event) {
     value.textContent = ++counterValue;
-    console.log(value);
 };
 additionBtn.addEventListener('click', onAdditionBtnClick);
